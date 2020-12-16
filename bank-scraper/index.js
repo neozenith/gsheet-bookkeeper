5ce38201ab86b5db7758692d27db895cc387eac8
Josh Peak <neozenith.dev@gmail.com>
2020-12-16 14:16:42 +1100

    Start work on puppeteer scraper

---
+++ bank-scraper/index.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+const CREDS = require('./credentials.json');
+const options = {headless: false};
+
+(async () => {
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+  await page.goto(CREDS.host, {waitUntil: 'networkidle2'});
+  await page.screenshot({path: 'example.png'});
+  await page.type('#txtMyClientNumber_field', CREDS.username);
+  await page.type('#txtMyPassword_field', CREDS.password);
+  // click and wait for navigation
+  await Promise.all([
+    page.click('#btnLogon_field'),
+    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+  ]);
+  await page.screenshot({path: 'example2.png'});
+
+
+  await browser.close();
+})();
